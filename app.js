67453c12c933218de094c8f1f2743bc2e5c2bed2
Александр Трофимов <trofimoff@live.ru>
2019-04-09 06:27:35 +0300

    --

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ $(document).ready(function() {
     url: './images/album.json5',
     method: 'get',
     dataType: 'text',
-    success: function(data) {
+    success: function(data) {  
+      data  = JSON5.parse(data);    
+      document.title = data.album.title;
+      
       let ul    = $('<ul/>').addClass('album').appendTo('.content'),
-          album = JSON5.parse(data);
-          
-      album = Object.assign({}, album.album.image);
+          album = Object.assign({}, data.album.image);
   
       $.each(album, function(i, entry) {
         let li = $('<li/>').appendTo(ul),
-            a  = $('<a/>').attr({ href: 'javascript:void(0)' }).appendTo(li);
+            a  = $('<a/>').attr({ href: entry }).appendTo(li);
         
         $('<img/>').attr({
             src: entry,
-            width:  512,
-            height: 512
+            width:  128,
+            height: 128
         }).appendTo(a);
+        
+        a.click(function() {
+          return false;
+        });
       });
     }
   }); 
